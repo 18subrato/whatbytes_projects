2d6c979731647893d9fb6628116443e8356824ff
18subrato <subratoverma7@gmail.com>
2025-07-18 02:19:20 +0530

    added all logic

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Image from "next/image";
 import useProduct from "@/context/ProductContext";
 
 function Items({ image, id, title, price }) {
@@ -8,14 +8,15 @@ function Items({ image, id, title, price }) {
     const handleAdd = () => {
         AddToCart({ id });
         setFleg(true);
+        alert('Added to cart');
     };
 
     return (
         <div className="border p-3">
-            <img src={image} className="w-full h-[200px] object-contain" alt={title} />
+            <Image src={image} className="w-full h-[200px] object-contain" alt={title} />
             <h2>{title}</h2>
             <p>${price}</p>
-            <button onClick={handleAdd} className="mt-2 border p-2">Add to Cart</button>
+            <button onClick={handleAdd} className="mt-2 border p-2 cursor-pointer">Add to Cart</button>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 'use client'
 import useProduct from "@/context/ProductContext";
 import { Search, ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 
 
@@ -10,17 +11,19 @@ function Navbar() {
     return(
         <div className="bg-blue-600 max-w-screen flex px-10 py-8 justify-between">
             <div>
-                <h3 className="text-4xl text-white font-bold ">LOGO</h3>
+                <h3 className="md:text-4xl text-xl text-white font-bold ">LOGO</h3>
             </div>
             <div className="flex gap-10">
                 <div className=" relative hidden lg:block">
                     <Search className="absolute top-1.5 lg:top-2.5 left-1.5 text-white "/>
                     <input type="text" onChange={(e) => setSearch(e.target.value)} value={search} placeholder="Search for Products..." className="border-2 border-blue-400 rounded-md w-[300px] xl:w-[400px] pl-10 p-2 outline-none text-white"/>
                 </div>
-                <div className=" relative lg:hidden">
+                <div className=" relative mr-4 lg:hidden">
                     <Search onClick={() => setShowSearch(!showSearch)} className="absolute top-1.5 lg:top-2.5 left-1.5 text-white "/>
                 </div>
-                <button onClick={() => navigate('/cart')} className="flex bg-blue-950 justify-center items-center px-10 font-bold rounded-md text-white gap-2 cursor-pointer"><ShoppingCart/><span>Cart</span></button>
+                <Link href='/cart'>
+                <button className="flex bg-blue-950 justify-center items-center md:px-10 px-4 py-3 font-bold rounded-md text-white gap-2 cursor-pointer"><ShoppingCart/><span>Cart</span></button>
+                </Link>
             </div>
         </div>
     )
