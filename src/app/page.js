6358ff1385918e661c2edd5aa66a4c4add49fe42
18subrato <subratoverma7@gmail.com>
2025-07-17 19:51:45 +0530

    create footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Footer from '@/components/layout/Footer'
 import Navbar from '@/components/layout/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-screen lg:px-20 xl:px-30'>
+    <div className='max-w-screen lg:px-20 xl:px-30'>
       <Navbar/>
+      <Footer/>
     </div>
   )
 }
